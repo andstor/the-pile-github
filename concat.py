2f72bc960916816d6fc8347be8074960ed7638ce
André Storhaug <andr3.storhaug@gmail.com>
2023-03-11 15:31:05 +0100

    Add script for concatenation

---
+++ concat.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from glob import glob
+from pathlib import Path
+
+for i in range(0, 30):
+    print(f"Processing data{i:02d}")
+    languages = {}
+    globbed_files = glob(f"data{i:02d}/**/*.parquet", recursive=True)
+
+    # sort 
+    #globbed_files.sort( key = lambda x: x.split('/')[0])
+    globbed_files.sort()
+    print(globbed_files)
+    for file in globbed_files:
+        path = Path(file)
+        language = path.parent.parent.name
+        languages.setdefault(language, [])
+        languages[language].append(file)
+
+    for language, files in languages.items():
+        print(language, len(files))
+        df = pd.concat([pd.read_parquet(file) for file in files])
+        print(df.shape)
+        # mkdir
+        save_path = Path(f"data/{language}")
+        save_path.mkdir(exist_ok=True, parents=True)
+        df.to_parquet(save_path / "train.parquet", )
